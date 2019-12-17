e3ec439f6f4991752556d478981215486f43a5c8
Guruh Edi Purwanto <nagaremono@tutanota.com>
2019-12-17 20:30:49 +0800

    added clear and new grid button and function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,16 +2,45 @@
 
 const container = document.querySelector('.grid');
 
-for (let i = 0; i < (16*16); i++) {
-  var box = document.createElement('div');
-  box.classList.add('box');
-  container.appendChild(box);
+load()
+
+function load(size = 16) {
+  for (let i = 0; i < (size*size); i++) {
+    var box = document.createElement('div');
+    box.classList.add('box');
+    container.appendChild(box);
+  }
+  
+  const boxes = Array.from(document.querySelectorAll('.box'))
+  boxes.forEach((box) => {
+    box.addEventListener('mouseenter', (e) => {
+      e.target.style.backgroundColor = '#1e1e1e'
+    });
+  });
+} 
+
+function clear() {
+  const boxes = Array.from(document.querySelectorAll('.box'))
+  boxes.forEach((box) => {
+      box.style.backgroundColor = '#dfdede';
+  });
 }
 
-const boxes = Array.from(document.querySelectorAll('.box'))
-boxes.forEach((box) => {
-  box.addEventListener('mouseenter', (e) => {
-    e.target.style.backgroundColor = '#1e1e1e'
-  })
-})
+function createNewGrid() {
+  var size = prompt('Input size for new grid', 0)
+  while (container.firstChild) {
+    container.removeChild(container.firstChild)
+  }
+
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+  load(size)
+}
+
+const newButton = document.querySelector('.new')
+newButton.addEventListener('click', createNewGrid)
+
+const clearButton = document.querySelector('.clear')
+clearButton.addEventListener('click', clear)
 
